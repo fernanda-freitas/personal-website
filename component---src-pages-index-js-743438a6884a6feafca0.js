22d2cd58500621445dde2f24aac9f4df32af6b44
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-04-30 13:04:28 +0100

    Updates

---
+++ component---src-pages-index-js-743438a6884a6feafca0.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{704:function(e,t,l){"use strict";l.r(t),l.d(t,{default:function(){return n}});var r=l(294);function n(){return r.createElement("div",null,"Hello world! Teste")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-743438a6884a6feafca0.js.map
