b7d5ef65b189358678c361da2845847b6e1a33f8
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-04-30 20:25:52 +0100

    inicial structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,10 @@ import React from "react"
 import Layout from "../components/layout"
 import Link from 'gatsby-link'
 
-
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <Layout>
-      <Link to="/about/">Link to About</Link>
+        <Link to="/">Link to home</Link>
     </Layout>
   )
 }
