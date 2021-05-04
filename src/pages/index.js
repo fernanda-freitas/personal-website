26c7f46936333cf6867c33a9444baacbcab1d20a
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-05-04 09:58:34 +0100

    react particles js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,68 @@
 import React from "react"
 import Layout from "../components/layout"
-import Link from 'gatsby-link'
 import "tailwindcss/tailwind.css"
+import Particles from 'react-particles-js';
 
 
 export default function HomePage() {
   return (
     <Layout>
-      <div className="bg-gray-200 h-96"></div>
+      <div style={{backgroundColor: "#F9FAFB"}}>
+      <Particles
+          params={{
+            "particles": {
+                "number": {
+                    "value": 150,
+                    "density": {
+                        "enable": true
+                    }
+                },
+                "size": {
+                    "value": 4,
+                    "random": true,
+                    "anim": {
+                        "speed": 0,
+                        "size_min": 0.3
+                    }
+                },
+                "color": {
+                  "value": "#374151"
+                },
+                "line_linked": {
+                    "enable": false
+                },
+                "move": {
+                    "random": true,
+                    "speed": 2,
+                    "out_mode": "in"
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "repulse"
+                    }
+                },
+                "modes": {
+                    "bubble": {
+                        "distance": 250,
+                        "duration": 2,
+                        "size": 0,
+                        "opacity": 1
+                    },
+                    "repulse": {
+                        "distance": 150,
+                        "duration": 4
+                    }
+                }
+            }
+        }} />
+      </div>
     </Layout>
   )
 }
