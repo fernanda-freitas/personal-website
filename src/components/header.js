7f022a250527d1347291b7221fa53ae8fa13c733
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-05-05 11:34:22 +0100

    header and hero adj

--- src/components/header.js
+++ src/components/header.js
@@ -7,12 +7,12 @@ export default function Header() {
         <div>
             <div className="absolute inset-0 z-10 w-10/12 h-20 flex mx-auto items-center justify-between font-rubik text-gray-700">
                 <div>
-                    <Link to="..//" className="text-xl font-medium">@fernanda-martinez</Link>
+                    <Link to="..//" className="text-xl font-medium">@fernanda-freitas</Link>
                 </div>
                 <div className="space-x-6 md:space-x-12 text-sm">
-                    <Link to="../about/" className="uppercase">Work</Link>
-                    <Link to="../about/" className="uppercase">About</Link>
-                    <Link to="../about/" className="uppercase">Contact</Link>
+                    <Link to="../about/" className="font-rubik uppercase hover:font-medium">Work</Link>
+                    <Link to="../about/" className="font-rubik uppercase hover:font-medium">About</Link>
+                    <Link to="../about/" className="font-rubik uppercase hover:font-medium">Contact</Link>
                 </div>
             </div>
         </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
-import Link from 'gatsby-link'
+
 
 export default function AboutPage() {
   return (
     <Layout>
-        <Link to="/">Link to home</Link>
+
     </Layout>
   )
 }
