9cdba92c9776c98d16001440558b93ac2b3d6766
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-05-07 09:49:50 +0100

    header particles

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,22 +6,6 @@ import Particles from 'react-particles-js';
 export default function Hero() {
     return (
         <div>
-          <div className="grid grid-cols-12 h-screen w-10/12 mx-auto pt-40 md:pt-60">
-            <div className="col-span-12 md:col-span-3">
-              <h1 className="text-6xl font-rubik font-bold text-gray-700 uppercase">Hello,</h1>
-            </div>
-            <div className="col-span-12 md:col-span-9 pt-20 md:pt-0">
-              <h3 className="text-4xl font-rubik font-light leading-normal text-gray-700">I’m a <span className="font-semibold">UI/UX Designer</span> who creates <span className="font-semibold">digital interfaces</span>, <span className="font-semibold">prototypes</span> and <span className="font-semibold">front-ends it</span>. Apart from that I love to illustrate and create beautiful visual concepts.</h3>
-            </div>
-            <div className="align-bottom">
-              <span className="font-rubik text-sm text-gray-700 uppercase">See Projects</span>
-              <Link to="/">
-                <svg className="animate-bounce mt-10" xmlns="http://www.w3.org/2000/svg" width="17.847" height="36.207" viewBox="0 0 17.847 36.207">
-                  <path id="Caminho_149" data-name="Caminho 149" d="M23.019,29.283l-7.51,7.51m0,0L8,29.283m7.51,7.51V3" transform="translate(-6.586 -2)" fill="none" stroke="#4b5563" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                </svg>
-              </Link>
-            </div>
-          </div>
           <Particles
             className="absolute inset-0"
             params={{
@@ -37,7 +21,7 @@ export default function Hero() {
                     "value": "#9ca3af"
                   },
                   "size": {
-                      "value": 0,
+                      "value": 3,
                       "random": true,
                       "anim": {
                           "speed": 3,
@@ -45,7 +29,7 @@ export default function Hero() {
                       }
                   },
                   "line_linked": {
-                    "enable": true,
+                    "enable": false,
                     "distance": 150,
                     "color": "#d1d5db",
                     "opacity": 0.4,
@@ -92,6 +76,22 @@ export default function Hero() {
                 }
               }
           }} />
+          <div className="grid grid-cols-12 h-screen w-10/12 mx-auto pt-40 md:pt-60">
+            <div className="col-span-12 md:col-span-3">
+              <h1 className="text-6xl font-rubik font-bold text-gray-700 uppercase">Hello,</h1>
+            </div>
+            <div className="col-span-12 md:col-span-9 pt-20 md:pt-0">
+              <h3 className="text-4xl font-rubik font-light leading-normal text-gray-700">I’m a <span className="font-semibold">UI/UX Designer</span> who creates <span className="font-semibold">digital interfaces</span>, <span className="font-semibold">prototypes</span> and <span className="font-semibold">front-ends it</span>. Apart from that I love to illustrate and create beautiful visual concepts.</h3>
+            </div>
+            <div className="align-bottom">
+              <span className="font-rubik text-sm text-gray-700 uppercase">See Projects</span>
+              <Link to="/">
+                <svg className="animate-bounce mt-10" xmlns="http://www.w3.org/2000/svg" width="17.847" height="36.207" viewBox="0 0 17.847 36.207">
+                  <path id="Caminho_149" data-name="Caminho 149" d="M23.019,29.283l-7.51,7.51m0,0L8,29.283m7.51,7.51V3" transform="translate(-6.586 -2)" fill="none" stroke="#4b5563" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                </svg>
+              </Link>
+            </div>
+          </div>
       </div>
     )
 };
