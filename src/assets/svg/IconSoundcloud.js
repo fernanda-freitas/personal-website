ebe7de428d0fbe14f9951d90b040d270538e67a1
Fernanda freitas Martinez <fernanda.martinez@integer.pt>
2021-05-09 23:20:14 +0100

    about section

--- src/assets/svg/IconSoundcloud.js
+++ src/assets/svg/IconSoundcloud.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function IconSoundcloud() {
   return (
-    <svg id="soundcloud" xmlns="http://www.w3.org/2000/svg" width="39.366" height="19.682" viewBox="0 0 39.366 19.682">
+    <svg id="soundcloud" xmlns="http://www.w3.org/2000/svg" width="39.366" height="19.682" viewBox="0 0 40 19">
       <path id="Caminho_156" data-name="Caminho 156" d="M9.842,92.516H12.3V77.023a6.326,6.326,0,0,0-2.46,1.357ZM4.921,82.035V92.5l.159.017h2.3v-10.5h-2.3ZM0,87.265a5.271,5.271,0,0,0,2.46,4.479V82.791A5.265,5.265,0,0,0,0,87.265Zm14.762,5.25h2.46V77.706a6.142,6.142,0,0,0-2.46-.878Zm19.524-10.5H32.9a8.071,8.071,0,0,0,.114-1.311,7.749,7.749,0,0,0-7.62-7.873,7.477,7.477,0,0,0-5.714,2.7V92.516h14.6a5.251,5.251,0,0,0,0-10.5Z" transform="translate(0 -72.834)" fill="#9ca3af"/>
     </svg>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,70 +22,70 @@ export default function HomePage() {
         <Hero></Hero>
         <Work></Work>
         <Cursor />
-        <section id="about" className="h-screen bg-gray-100">
-          <div className="w-10/12 mx-auto py-40 grid grid-cols-12 grid-rows-2 gap-8">
-              <h1 className="col-span-12 row-span-1 lg:col-span-3 text-5xl font-rubik font-bold text-gray-900">About</h1>
-              <div className="col-span-12 row-span-1 lg:col-span-8">
+        <section id="about" className="bg-gray-100">
+          <div className="w-10/12 mx-auto py-40 grid grid-cols-12 gap-8">
+              <h1 className="col-span-12 lg:col-span-3 text-5xl font-rubik font-bold text-gray-900">About</h1>
+              <div className="col-span-12 lg:col-span-8">
                 <p className="font-rubik text-lg text-gray-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec dui nunc. Ligula ullamcorper malesuada proin libero nunc. Vitae congue eu consequat ac felis donec et odio pellentesque. Tincidunt lobortis feugiat vivamus at augue. Turpis egestas maecenas pharetra convallis posuere. Integer vitae justo eget magna fermentum iaculis eu non.</p>
                 <p className="font-rubik text-lg text-gray-900">Risus viverra adipiscing at in tellus integer feugiat. Egestas egestas fringilla phasellus faucibus scelerisque. Ultricies leo integer malesuada nunc. Lacus sed viverra tellus in hac habitasse platea dictumst.</p>
+                <div className="flex space-x-6">
+                  <a 
+                  href="/" 
+                  target="_blank" 
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                  tabIndex="0"
+                  className="social-icon"
+                  >
+                    <IconBehance />
+                  </a>
+                  <a 
+                  href="/" 
+                  target="_blank" 
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                  tabIndex="0"
+                  className="social-icon"
+                  >
+                    <IconDribbble />
+                  </a>
+                  <a 
+                  href="/" 
+                  target="_blank" 
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                  tabIndex="0"
+                  className="social-icon"
+                  >
+                    <IconInstagram />
+                  </a>
+                  <a 
+                  href="/" 
+                  target="_blank" 
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                  tabIndex="0"
+                  className="social-icon"
+                  >
+                    <IconLinkedin />
+                  </a>
+                  <a 
+                  href="/" 
+                  target="_blank" 
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                  tabIndex="0"
+                  className="social-icon"
+                  >
+                    <IconSoundcloud />
+                  </a>
+                </div>
               </div>
-            <div className="flex col-start-4 col-span-8 row-start-2 space-x-6">
-              <a 
-              href="/" 
-              target="_blank" 
-              rel="noreferrer"
-              onMouseEnter={() => cursorChangeHandler("hovered")}
-              onMouseLeave={() => cursorChangeHandler("")}
-              tabIndex="0"
-              className="social-icon"
-              >
-                <IconBehance />
-              </a>
-              <a 
-              href="/" 
-              target="_blank" 
-              rel="noreferrer"
-              onMouseEnter={() => cursorChangeHandler("hovered")}
-              onMouseLeave={() => cursorChangeHandler("")}
-              tabIndex="0"
-              className="social-icon"
-              >
-                <IconDribbble />
-              </a>
-              <a 
-              href="/" 
-              target="_blank" 
-              rel="noreferrer"
-              onMouseEnter={() => cursorChangeHandler("hovered")}
-              onMouseLeave={() => cursorChangeHandler("")}
-              tabIndex="0"
-              className="social-icon"
-              >
-                <IconInstagram />
-              </a>
-              <a 
-              href="/" 
-              target="_blank" 
-              rel="noreferrer"
-              onMouseEnter={() => cursorChangeHandler("hovered")}
-              onMouseLeave={() => cursorChangeHandler("")}
-              tabIndex="0"
-              className="social-icon"
-              >
-                <IconLinkedin />
-              </a>
-              <a 
-              href="/" 
-              target="_blank" 
-              rel="noreferrer"
-              onMouseEnter={() => cursorChangeHandler("hovered")}
-              onMouseLeave={() => cursorChangeHandler("")}
-              tabIndex="0"
-              className="social-icon"
-              >
-                <IconSoundcloud />
-              </a>
-            </div>
           </div>
         </section>
         <section id="contact" className="w-10/12 mx-auto my-32 block md:flex items-end">
