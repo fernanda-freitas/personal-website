0826d9a1b82f8251cbdb0ca7c8fd13a7077bb283
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-05-25 10:34:54 +0100

    adjust work grid

--- src/components/work.js
+++ src/components/work.js
@@ -16,69 +16,67 @@ export default function Work() {
   const { cursorChangeHandler } = useContext(MouseContext);
 
     return (
-        <section id="work" className="w-10/12 mx-auto my-40">
-          <div className="grid grid-cols-12 gap-12 mt-20">
+        <section id="work" className="grid grid-cols-12 gap-8 min-h-172 w-10/12 mx-auto pt-20">
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <Link className="project-thumb text-gray-700 hover:scale transition duration-300 ease-in-out" to="/illustration/">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={IllustrationThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={IllustrationThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Illustrations</h3>
-                </Link>  
+                </Link>
             </div>
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <Link className="project-thumb text-gray-700 transition duration-300 ease-in-out" to="/transparencia/">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={TbThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={TbThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Transparência Brasil</h3>
-                </Link>  
+                </Link>
             </div>
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <Link className="project-thumb text-gray-700 transition duration-300 ease-in-out" to="/medica/">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={MedicaThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={MedicaThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Medica</h3>
-                </Link>  
+                </Link>
             </div>
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <Link className="project-thumb text-gray-700 transition duration-300 ease-in-out" to="/construir/">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={ConstruirThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={ConstruirThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Construir</h3>
-                </Link>  
+                </Link>
             </div>
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <Link className="project-thumb text-gray-700 transition duration-300 ease-in-out" to="/inohub/">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={InohubThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={InohubThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Inohub</h3>
-                </Link>  
+                </Link>
             </div>
             <div className="col-span-12 md:col-span-6"
                 onMouseEnter={() => cursorChangeHandler("hovered")}
                 onMouseLeave={() => cursorChangeHandler("")}>
                 <a href="https://dribbble.com/fernanda_freitas" target="_blank" className="project-thumb text-gray-700 transition duration-300 ease-in-out">
                   <div className="mb-6 rounded-3xl overflow-hidden" alt="Illustration project">
-                    <img className="object-cover w-full h-60 transform transition duration-500 hover:scale-110" src={DribbbleThumb} alt="Alterar alt"/>
+                    <img className="object-cover w-full h-40 md:h-60 transform transition duration-500 hover:scale-110" src={DribbbleThumb} alt="Alterar alt"/>
                   </div>
                     <h3 className="inline ml-8 text-xl font-rubik text-gray-700">Dribbble Shots</h3>
-                </a>  
+                </a>
             </div>
-          </div>
         </section>
     )
 };
