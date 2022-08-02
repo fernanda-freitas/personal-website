9c86682b34ac25730f7c07b08c084b1d13f401fd
fernanda freitas <idea.fernanda@gmail.com>
2022-08-02 12:02:36 +0100

    Updates

--- component---src-pages-about-js-20dafb32147bcc3e6c82.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[682],{198:function(e,t,n){"use strict";n.d(t,{Z:function(){return l}});var r=n(294);function l(e){var t=e.children;return r.createElement("div",null,r.createElement("main",null,t))}},518:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return a}});var r=n(294),l=n(198),u=n(799);function a(){return r.createElement(l.Z,null,r.createElement(u.ZP,{to:"/"},"Link to home"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-20dafb32147bcc3e6c82.js.map
