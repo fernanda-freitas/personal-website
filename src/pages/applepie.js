a347999e565f00e79ca0b5a798a611dd9be93c11
fernanda freitas <idea.fernanda@gmail.com>
2022-08-02 16:30:31 +0100

    add link to applepie app

--- src/pages/applepie.js
+++ src/pages/applepie.js
@@ -9,7 +9,8 @@ export default function applepie() {
         <>
           <div className="w-10/12 mx-auto mb-28 mt-32">
             <h2 className="text-5xl font-rubik font-bold text-gray-700">Applepie</h2>
-            <h2 className="max-w-5xl mt-3 text-5xl font-rubik leading-tight text-gray-600">Applepie is a marketplace app for buying and selling iPods. It was developed as part of a bootcamp using Rails.</h2>
+            <h2 className="max-w-5xl mt-3 text-5xl font-rubik leading-tight text-gray-600">
+              <a className="font-rubik text-purple-600" href="https://applepie-1976.herokuapp.com/" target="_blank">Applepie</a> is a marketplace app for buying and selling iPods. It was developed as part of a bootcamp using Rails.</h2>
             <div className="mt-6">
               <div className="flex flex-wrap gap-2">
                 <div class="inline bg-purple-100 text-purple-600 antialiased rounded-full text-sm tracking-wide font-semibold px-3 py-1 whitespace-nowrap">Interface design</div>
