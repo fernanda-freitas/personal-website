210466c4e33df3df6030ff6a55182318a8ab6d83
fernanda freitas <idea.fernanda@gmail.com>
2022-10-10 12:28:47 +0100

    new hero design and frontend

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,36 +4,50 @@ import IconArrowDown from "../assets/svg/IconArrowDown"
 import Cursor from ".//Cursor"
 import { useContext } from "react"
 import { MouseContext } from "../context/mouse-context"
+import ProfileImage from "../assets/images/profile.png"
+import IconTalk from "../assets/svg/IconTalk"
+import IconStar from "../assets/svg/IconStar"
+import IconGlobe from "../assets/svg/IconGlobe"
+import IconPin from "../assets/svg/IconPin"
 
 export default function Hero() {
   const { cursorChangeHandler } = useContext(MouseContext)
   return (
     <>
-      <div className="grid grid-cols-12 gap-8 min-h-172 w-10/12 mx-auto md:pt-20">
-        <div className="col-span-12 md:col-span-10 gap-8 pt-40 md:pt-60">
-          <h1 className="title text-5xl md:text-6xl font-rubik font-bold text-gray-700 uppercase">
-            Hello,
-          </h1>
-          <h3 className="introduction mt-8 font-rubik font-light text-gray-700">
-            I’m a <span className="font-semibold">Fullstack Dev student</span> with years of experience in <span className="font-semibold">Ux/Ui Design</span> (usability, wireframes, digital interfaces, prototypes and more recently, development) 🚀
-          </h3>
-          <h5 className="mt-6 text-xl md:text-1xl font-rubik font-light leading-relaxed text-gray-500">
-            I'm currently perfecting my Fullstack Dev skills with <a className="font-rubik text-purple-600" href="https://www.switchup.org/bootcamps/le-wagon" target="_blank"> Le Wagon's </a> super imersive Bootcamp - considered the best bootcamp according to
-            <a className="font-rubik text-purple-600" href="https://www.switchup.org/bootcamps/le-wagon" target="_blank"> Switchup </a>
-            and <a className="font-rubik text-purple-600" href="https://www.coursereport.com/schools/le-wagon" target="_blank">Course Report</a>.
-            This is the core of what I've beeng learning: Ruby, SQLite, Postgres SQL, JavaScript (ES6, AJAX, HTML, CSS (SCSS), Git & Github, Heroku.
-          </h5>
+      <div className="flex flex-col grid-cols-12 gap-8 w-10/12 mx-auto">
+        <div className="col-span-12 md:col-span-10 gap-8 pt-40 md:pt-60 text-center mx-auto">
+          <img className="w-auto h-36 md:h-40 mx-auto" src={ProfileImage} alt="Alterar alt"/>
+          <h4 className="mt-6 text-xl md:text-2xl font-rubik font-regular leading-relaxed text-gray-700">Frontend developer and Ux/Ui designer</h4>
+          <h1 className="title text-5xl md:text-6xl mt-4 font-rubik font-semibold text-gray-700">Fernanda Freitas</h1>
+          <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-12 mt-8">
+            <div className="flex space-x-1 mx-auto">
+              <IconPin />
+              <h5 className="md:text-xl font-rubik font-regular leading-relaxed text-gray-700">Lisbon, Portugal</h5>
+            </div>
+            <div className="flex space-x-1 mx-auto">
+              <IconTalk />
+              <h5 className="md:text-xl font-rubik font-regular leading-relaxed text-gray-700">English, Portuguese</h5>
+            </div>
+            <div className="flex space-x-1 mx-auto">
+              <IconStar />
+              <h5 className="md:text-xl font-rubik font-regular leading-relaxed text-gray-700">Open to projects</h5>
+            </div>
+            <div className="flex space-x-1 mx-auto">
+              <IconGlobe />
+              <h5 className="md:text-xl font-rubik font-regular leading-relaxed text-gray-700">Remote</h5>
+            </div>
+          </div>
         </div>
         <Cursor />
-        <div className="col-span-12 md:col-span-8 flex flex-col justify-end mb-10 text-gray-700">
-          <span className="font-rubik text-sm uppercase">See Projects</span>
+        <div className="col-span-12 md:col-span-8 flex flex-col mx-auto mb-10 text-gray-700">
           <div
             onMouseEnter={() => cursorChangeHandler("hovered")}
             onMouseLeave={() => cursorChangeHandler("")}
             role="link"
             tabIndex="0"
+            className="flex"
           >
-            <AnchorLink to="/#work" title="All works">
+            <AnchorLink to="/#work" title="All works" className="mx-auto">
               <IconArrowDown />
             </AnchorLink>
           </div>
